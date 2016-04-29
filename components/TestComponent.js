2036f245c7f222f323cdea930c9f1439d931a1d2
vplan <vplan@nootbyte.nl>
2016-04-29 14:28:08 +0200

    TestComponent

---
+++ components/TestComponent.js
@@ -0,0 +1,27 @@
+var noflo = require('noflo');
+
+exports.getComponent = function() {
+  var c = new noflo.Component();
+  c.description = 'Forwards packets and metadata in the same way it receives them';
+  c.icon = 'forward';
+  c.inPorts.add('in', {
+    datatype: 'all',
+    description: 'Packet to forward'
+  });
+  c.outPorts.add('out', {
+    datatype: 'all'
+  });
+  noflo.helpers.WirePattern(c, {
+    "in": ['in'],
+    out: 'out',
+    forwardGroups: true,
+    async: true
+  }, function(data, groups, out, callback) {
+    // do something with data
+    // send output
+    out.send(data);
+    // tell WirePattern we are done
+    return callback();
+  });
+  return c;
+};
